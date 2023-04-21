edf64a25eba35e236ff757332b3b5c85d44b9438
Gabriel Conceição <gabrielc2019@outlook.com.br>
2023-04-21 11:22:57 -0300

    esqueleto do projeto e lógica finalizados

--- src/Components/Board/index.jsx
+++ src/Components/Board/index.jsx
@@ -5,24 +5,62 @@ import './Board.css';
 export function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [player, setPlayer] = useState('');
-  const togglePlayer = () => {
-    setPlayer((prev) => (prev === 'X' ? 'O' : 'X'));
+  const [winner, setWinner] = useState({ win: false, player: '' });
+
+  const togglePlayer = () => setPlayer((prev) => (prev === 'X' ? 'O' : 'X'));
+
+  const randPlayer = () => {
+    const random = Math.floor(Math.random() * 2);
+
+    setPlayer(random === 0 ? 'X' : 'O');
+  };
+
+  const checkWinner = (square) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (square[a] && square[a] === square[b] && square[b] === square[c]) {
+        setWinner({ win: true, player });
+        return;
+      }
+    }
+    if (square.every((el) => el !== null)) {
+      setWinner({ win: '', player: '' });
+    }
   };
 
   const handleClick = (id) => {
     const newSquares = [...squares];
 
-    if (newSquares[id]) return;
+    if (newSquares[id] || winner.win) return;
 
     newSquares[id] = player;
     setSquares(newSquares);
-    togglePlayer();
+    checkWinner(newSquares);
+    if (!winner.win) {
+      togglePlayer();
+    }
+  };
+
+  const reset = () => {
+    setSquares(Array(9).fill(null));
+    randPlayer();
+    setWinner({ win: false, player: '' });
   };
 
   useEffect(() => {
-    const random = Math.floor(Math.random() * 2);
-
-    setPlayer(random === 0 ? 'X' : 'O');
+    randPlayer();
   }, []);
 
   return (
@@ -33,9 +71,26 @@ export function Board() {
         ))}
       </div>
 
-      <h3>
-        É a vez de: <span>{player}</span>
-      </h3>
+      <div className="score">
+        {winner.win ? (
+          <>
+            <h3>O vencedor é: {winner.player}</h3>
+            <button onClick={reset} type="button">
+              Jogar de novo
+            </button>
+          </>
+        ) : (
+          <h3>É a vez de: {player}</h3>
+        )}
+        {winner.win === '' && (
+          <>
+            <h3>Velha</h3>
+            <button onClick={reset} type="button">
+              Jogar de novo
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
